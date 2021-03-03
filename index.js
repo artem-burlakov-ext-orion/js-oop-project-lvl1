045b036de359c1d56d95ac0d8b6a65e6e1341d7f
Artem <bartem007@gmail.com>
2021-03-03 17:37:31 +0300

    add .contain() .required()

--- index.js
+++ index.js
@@ -4,26 +4,20 @@ export default class Validator {
   }
 
   string() {
-    return new Validator([...this.constraints, (arg) => typeof arg === 'string'])
+    return new Validator([...this.constraints, (arg) => typeof arg === 'string']);
   }
 
-  // required() {
-  //   return new Validator(
-  //     {
-  //       ...this.constraints,
-  //       required: (arg) => arg.length > 0,
-  //     }
-  //   )
-  // }
+  required() {
+    this.constraints = [...this.constraints, (arg) => arg ? arg.length > 0 : false];
+    return new Validator(this.constraints);
+  }
+
+  contains(substr) {
+    this.constraints = [...this.constraints, (arg) => arg.includes(substr)];
+    return new Validator(this.constraints);
+  }
   
   isValid(arg) {
     return this.constraints.map((c) => c(arg)).every((el) => el === true);
     }
 }
-
-// schema.isValid('hexlet'); // true
-// schema.isValid(null); // false
-// schema.isValid(''); // false
-
-// schema.contains('what').isValid('what does the fox say'); // true
-// schema.contains('whatthe').isValid('what does the fox say'); // false
---
+++ run.js
@@ -0,0 +1,6 @@
+let arr = [];
+
+arr = [...arr,(arg)  => arg];
+arr = [...arr,(arg) => arg * 6];
+
+console.log(arr);
