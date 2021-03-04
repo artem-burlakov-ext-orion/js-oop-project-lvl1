82840744ae06f2329207f840d49084ccf973d374
Artem <bartem007@gmail.com>
2021-03-04 15:25:06 +0300

    add object .shape

--- src/Validator.js
+++ src/Validator.js
@@ -2,34 +2,51 @@ export default class Validator {
   constructor(constraints = [], type = '') {
     this.constraints = constraints;
     this.type = type;
+    this.isNullValid = true;
+  }
+
+  shape(schema) {
+    if (this.type === 'object') {
+      this.constraints = Object.entries(schema).map(([key, value]) => {
+        const { constraints, type } = value;
+        return {
+          prop: key,
+          constraints,
+          type,
+        }
+      });
+      return new Validator(this.constraints, this.type);
+    }
+  }
+
+  object() {
+    return new Validator(this.constraints, 'object');
   }
 
   string() {
-    this.type = 'string';
-    return new Validator([...this.constraints, (arg) => typeof arg === 'string'], this.type);
+    return new Validator([...this.constraints, (arg) => typeof arg === 'string'], 'string');
   }
 
   number() {
-    this.type = 'number';
-    return new Validator([...this.constraints, (arg) => typeof arg === 'number'], this.type);
+    return new Validator([...this.constraints, (arg) => typeof arg === 'number'], 'number');
   }
 
   array() {
-    this.type = 'array';
-    return new Validator([...this.constraints, (arg) => typeof arg === 'number'], this.type);
+    return new Validator([...this.constraints, 'array']);
   }
 
-
-  
   required() {
     switch (this.type) {
       case 'string':
+        this.isNullValid = false;
         this.constraints = [...this.constraints, (arg) => (arg ? arg.length > 0 : false)];
         return new Validator(this.constraints, this.type);
       case 'number':
+        this.isNullValid = false;
         this.constraints = [...this.constraints, (arg) => !!arg];
         return new Validator(this.constraints, this.type);
       case 'array':
+        this.isNullValid = false;
         this.constraints = [...this.constraints, (arg) => arg.isArray()];
         return new Validator(this.constraints, this.type);
       default:
@@ -58,6 +75,36 @@ export default class Validator {
   }
 
   isValid(arg) {
+    if (this.type = 'object') {
+      const res = Object.entries(arg).map(([key, value]) => {
+        const p = this.constraints.find((c) => c.prop === key);
+        return p.constraints.map((c) => {
+          if (value === null && this.isNullValid) {
+            return true;
+          }
+          if (value === null && !this.isNullValid) {
+            return false;
+          }
+          return c(value);
+        }).every((el) => el === true);
+      });
+      return res.every((el) => !!el);
+    }
     return this.constraints.map((c) => c(arg)).every((el) => el === true);
   }
 }
+
+const v = new Validator();
+
+const schema = v.object();
+
+// Позволяет описывать валидацию для свойств объекта
+schema.shape({
+  name: v.string().required(),
+  age: v.number().positive(),
+});
+
+console.log(schema.isValid({ name: 'kolya', age: 100 }))
+console.log(schema.isValid({ name: 'maya', age: null })); // true
+console.log(schema.isValid({ name: '', age: null })); // false
+console.log(schema.isValid({ name: 'ada', age: -5 })); // false
