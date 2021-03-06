65ca5c4e62900bd4a954b9cd1db225ebc270b2cc
Artem <bartem007@gmail.com>
2021-03-06 12:33:03 +0300

    add custom validators

--- src/Validator.js
+++ src/Validator.js
@@ -74,7 +74,29 @@ export default class Validator {
     return new Validator(this.constraints, this.type);
   }
 
+
+  addValidator(type, methodName, fn) {
+    this.constraints = ([...this.constraints, { method: methodName, f: fn }]);
+    return new Validator(this.constraints, type);
+  }
+
+  test(methodName, arg) {
+    const custom = this.constraints.find((c) => typeof c === 'object' && c.method === methodName);
+    custom['fnArg'] = arg;
+    return new Validator([...this.constraints, custom], this.type)
+  }
+
   isValid(arg) {
+    const custom = this.constraints.filter((c) => typeof c === 'object');
+    
+    if (custom.length > 0) {
+      const res = custom.map((obj) => {
+        const { f, fnArg } = obj;
+        return f(arg, fnArg);
+      });
+      return res.every((el) => !!el);
+    }
+
     if (this.type = 'object') {
       const res = Object.entries(arg).map(([key, value]) => {
         const p = this.constraints.find((c) => c.prop === key);
@@ -96,15 +118,19 @@ export default class Validator {
 
 const v = new Validator();
 
-const schema = v.object();
+// let fn = (value, start) => value.startsWith(start);
+// // Метод добавления новых валидаторов
+// // addValidator(type, name, fn)
+// v.addValidator('string', 'startWith', fn);
 
-// Позволяет описывать валидацию для свойств объекта
-schema.shape({
-  name: v.string().required(),
-  age: v.number().positive(),
-});
+// // Новые валидаторы вызываются через метод test
+// const schema = v.string().test('startWith', 'H');
+// console.log(schema.isValid('exlet')); // false
+// console.log(schema.isValid('Hexlet')); // true
 
-console.log(schema.isValid({ name: 'kolya', age: 100 }))
-console.log(schema.isValid({ name: 'maya', age: null })); // true
-console.log(schema.isValid({ name: '', age: null })); // false
-console.log(schema.isValid({ name: 'ada', age: -5 })); // false
+const fn = (value, min) => value >= min;
+v.addValidator('number', 'min', fn);
+
+const schema = v.number().test('min', 5);
+console.log(schema.isValid(4)); // false
+console.log(schema.isValid(6)); // true
