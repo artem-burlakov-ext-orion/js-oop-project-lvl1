428458c3e7f36c5dbeb8d5471fde3ee5c74d137c
Artem <bartem007@gmail.com>
2021-03-06 23:54:33 +0300

    fix eslint errors

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -5,126 +5,110 @@ it('should be instanceOf Validator', () => {
 });
 
 it('should return instanceOf Validator after .string()', () => {
-  expect(new Validator().string()).toBeInstanceOf(Validator)
-})
+  expect(new Validator().string()).toBeInstanceOf(Validator);
+});
 
-it(`should return true because typeof '' === string`, () => {
+it('should return true because typeof \'\' === string', () => {
   expect(new Validator().string().isValid('')).toBe(true);
-})
+});
 
-it(`should return true because 'required' does notexist`, () => {
+it('should return true because \'required\' does notexist', () => {
   expect(new Validator().string().isValid(null)).toBe(true);
-})
+});
 
 it('should return instanceOf Validator after methods chaining', () => {
-  expect(new Validator().string().required()).toBeInstanceOf(Validator)
-})
+  expect(new Validator().string().required()).toBeInstanceOf(Validator);
+});
 
-it(`should return true because typeof 'string' === string and its length gt 0`, () => {
+it('should return true because typeof \'string\' === string and its length gt 0', () => {
   expect(new Validator().string().required().isValid('string')).toBe(true);
-})
+});
 
-it(`should return false because 'required' need string.length gt 0`, () => {
+it('should return false because \'required\' need string.length gt 0', () => {
   expect(new Validator().string().required().isValid('')).toBe(false);
-})
+});
 
-it(`should return false because 'required' exist`, () => {
+it('should return false because \'required\' exist', () => {
   expect(new Validator().string().required().isValid(null)).toBe(false);
-})
+});
 
-it(`should return false because 5 is not string`, () => {
+it('should return false because 5 is not string', () => {
   expect(new Validator().string().isValid(5)).toBe(false);
-})
+});
 
-it(`should return true because value contain argument`, () => {
+it('should return true because value contain argument', () => {
   expect(new Validator().string().contains('what').isValid('what does the fox say')).toBe(true);
-})
+});
 
-it(`should return false because value not contain argument`, () => {
+it('should return false because value not contain argument', () => {
   expect(new Validator().string().contains('whatthe').isValid('what does the fox say')).toBe(false);
-})
+});
 
 it('should return instanceOf Validator because .number()', () => {
-  expect(new Validator().number()).toBeInstanceOf(Validator)
-})
+  expect(new Validator().number()).toBeInstanceOf(Validator);
+});
 
-it(`should return true because required does not exist`, () => {
+it('should return true because required does not exist', () => {
   expect(new Validator().number().isValid(null)).toBe(true);
-})
+});
 
 it('should return instanceOf Validator after .number().required()', () => {
-  expect(new Validator().number().required()).toBeInstanceOf(Validator)
-})
+  expect(new Validator().number().required()).toBeInstanceOf(Validator);
+});
 
-it(`should return false because .required()`, () => {
+it('should return false because .required()', () => {
   expect(new Validator().number().required().isValid(null)).toBe(false);
-})
+});
 
-it(`should return true because typeof 5 === number and 5 > 0`, () => {
+it('should return true because typeof 5 === number and 5 > 0', () => {
   expect(new Validator().number().positive().isValid(5)).toBe(true);
-})
+});
 
-it(`should return false because -1 < 0`, () => {
+it('should return false because -1 < 0', () => {
   expect(new Validator().number().positive().isValid(-1)).toBe(false);
-})
+});
 
-it(`should return true because typeof 5 === number and 5 in range 1, 10`, () => {
+it('should return true because typeof 5 === number and 5 in range 1, 10', () => {
   expect(new Validator().number().range(1, 10).isValid(5)).toBe(true);
-})
+});
 
-it(`should return false because 5 not in range 6, 10`, () => {
+it('should return false because 5 not in range 6, 10', () => {
   expect(new Validator().number().range(6, 10).isValid(5)).toBe(false);
-})
+});
 
-it(`should return true because '[]' is array`, () => {
+it('should return true because \'[]\' is array', () => {
   expect(new Validator().array().required().isValid([])).toBe(true);
-})
+});
 
-it(`should return true because ['a', 'b'].length === 2`, () => {
+it('should return true because [\'a\', \'b\'].length === 2', () => {
   expect(new Validator().array().sizeOf(2).isValid(['a', 'b'])).toBe(true);
-})
+});
 
-it(`should return false because ['a', 'b'].length !== 1`, () => {
+it('should return false because [\'a\', \'b\'].length !== 1', () => {
   expect(new Validator().array().sizeOf(1).isValid(['a', 'b'])).toBe(false);
-})
+});
 
-it(`should return false because 'required' does not exist`, () => {
+it('should return false because \'required\' does not exist', () => {
   expect(new Validator().array().isValid(null)).toBe(false);
-})
+});
 
-it(`should return true`, () => {
+it('should return true1', () => {
   expect(new Validator().object().shape({
     name: new Validator().string().required(),
     age: new Validator().number().positive(),
   }).isValid({ name: 'kolya', age: 100 })).toBe(true);
-})
+});
 
-it(`should return true`, () => {
+it('should return true2', () => {
   expect(new Validator().object().shape({
     name: new Validator().string().required(),
     age: new Validator().number().positive(),
   }).isValid({ name: 'maya', age: null })).toBe(true);
-})
+});
 
-it(`should return false`, () => {
+it('should return false', () => {
   expect(new Validator().object().shape({
     name: new Validator().string().required(),
     age: new Validator().number().positive(),
   }).isValid({ name: '', age: null })).toBe(false);
-})
-
-
-// const v = new Validator();
-
-// const schema = v.object();
-
-// // Позволяет описывать валидацию для свойств объекта
-// schema.shape({
-//   name: v.string().required(),
-//   age: v.number().positive(),
-// });
-
-// schema.isValid({ name: 'kolya', age: 100 }); // true
-// schema.isValid({ name: 'maya', age: null }); // true
-// schema.isValid({ name: '', age: null }); // false
-// schema.isValid({ name: 'ada', age: -5 }); // false
+});
--- src/Validator.js
+++ src/Validator.js
@@ -1,13 +1,13 @@
 export default class Validator {
   constructor(constraints = {
-    nullValidation: (value) => true,
+    nullValidation: () => true,
   }, type = '') {
     this.constraints = constraints;
     this.type = type;
   }
 
   string() {
-    this.constraints['isString'] = (value) => typeof value === 'string';
+    this.constraints.isString = (value) => typeof value === 'string';
     return new Validator(this.constraints, 'string');
   }
 
@@ -19,23 +19,24 @@ export default class Validator {
     this.constraints.nullValidation = (value) => value !== null;
     return new Validator(this.constraints, 'array');
   }
-  
+
   object() {
+    this.constraints.nullValidation = (value) => value !== null;
     return new Validator(this.constraints, 'object');
   }
 
   required() {
     switch (this.type) {
       case 'string':
-        this.constraints['required'] = (value) => value.length > 0;
+        this.constraints.required = (value) => value.length > 0;
         this.constraints.nullValidation = (value) => value !== null;
         return new Validator(this.constraints, this.type);
       case 'number':
-        this.constraints['required'] = (value) => typeof value === 'number';
+        this.constraints.required = (value) => typeof value === 'number';
         this.constraints.nullValidation = (value) => value !== null;
         return new Validator(this.constraints, this.type);
       case 'array':
-        this.constraints['required'] = (value) => Array.isArray(value);
+        this.constraints.required = (value) => Array.isArray(value);
         return new Validator(this.constraints, this.type);
       default:
         throw new Error('uknown type');
@@ -43,22 +44,22 @@ export default class Validator {
   }
 
   contains(substr) {
-    this.constraints['isContains'] = (value) => value.includes(substr);
+    this.constraints.isContains = (value) => value.includes(substr);
     return new Validator(this.constraints, this.type);
   }
 
   positive() {
-    this.constraints['isPositive'] = (value) => value > 0;
+    this.constraints.isPositive = (value) => value > 0;
     return new Validator(this.constraints, this.type);
   }
 
   range(min, max) {
-    this.constraints['isInRange'] = (value) => value >= min && value <= max;
+    this.constraints.isInRange = (value) => value >= min && value <= max;
     return new Validator(this.constraints, this.type);
   }
 
   sizeOf(length) {
-    this.constraints['lengthValidate'] = (value) => value.length === length;
+    this.constraints.lengthValidate = (value) => value.length === length;
     return new Validator(this.constraints, this.type);
   }
 
@@ -73,69 +74,27 @@ export default class Validator {
   }
 
   test(methodName, arg) {
-    this.constraints[methodName]['arg'] = arg;
+    this.constraints[methodName].arg = arg;
     return new Validator(this.constraints, this.type);
   }
 
-
   isValid(value) {
-    console.log(value);
-    if (this.type === 'object') {
-      return Object.entries(value).map(([prop, val]) => {
-        return val === null
-          ? this.constraints[prop].constraints.nullValidation(val)
-          : Object.values(this.constraints[prop].constraints).map((c) => c(val)).every(el => el);
-      }).every(el => el);
+    if (value === null) {
+      return this.constraints.nullValidation(value);
     }
-    return value === null
-      ? this.constraints.nullValidation(value)
-      : Object.values(this.constraints).reduce((acc, c) => {
-        if (typeof c === 'object') {
-          return c.fn(value, c.arg) ? acc : false;
-        }
-        return c(value) ? acc : false
-      }, true );
+    if (this.type === 'object') {
+      return Object.entries(value).map(([prop, val]) => (val === null
+        ? this.constraints[prop].constraints.nullValidation(val)
+        : Object.values(this.constraints[prop].constraints)
+          .map((c) => c(val))
+          .every((el) => el)))
+        .every((el) => el);
+    }
+    return Object.values(this.constraints).reduce((acc, c) => {
+      if (typeof c === 'object') {
+        return c.fn(value, c.arg) ? acc : false;
+      }
+      return c(value) ? acc : false;
+    }, true);
   }
 }
-
-
-  // addValidator(type, methodName, fn) {
-  //   this.constraints = ([...this.constraints, { method: methodName, f: fn }]);
-  //   return new Validator(this.constraints, type);
-  // }
-
-  // test(methodName, arg) {
-  //   const custom = this.constraints.find((c) => typeof c === 'object' && c.method === methodName);
-  //   custom['fnArg'] = arg;
-  //   return new Validator([...this.constraints, custom], this.type)
-  // }
-
-  // isValid(arg) {
-  //   const custom = this.constraints.filter((c) => typeof c === 'object');
-
-  //   if (custom.length > 0) {
-  //     const res = custom.map((obj) => {
-  //       const { f, fnArg } = obj;
-  //       return f(arg, fnArg);
-  //     });
-  //     return res.every((el) => !!el);
-  //   }
-  //   if (this.type === 'object') {
-  //     console.log('!!!!');
-  //     const res = Object.entries(arg).map(([key, value]) => {
-  //       const p = this.constraints.find((c) => c.prop === key);
-  //       return p.constraints.map((c) => {
-  //         if (value === null && this.isNullValid) {
-  //           return true;
-  //         }
-  //         if (value === null && !this.isNullValid) {
-  //           return false;
-  //         }
-  //         return c(value);
-  //       }).every((el) => el === true);
-  //     });
-  //     return res.every((el) => !!el);
-  //   }
-  //   return this.constraints.map((c) => c(arg)).every((el) => el === true);
-  // }
-
